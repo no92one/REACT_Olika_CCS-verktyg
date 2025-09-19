dca1e0299894b14e9f75f636abe05c507df00d0c
unknown <linus.lindroth.92@gmail.com>
2025-09-19 10:42:18 +0200

    added custome button in tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 export default function App() {
   return <>
     <h1 class="text-5xl font-bold text-center">Homepage</h1>
-    
+    {/* 
+      "my-btn" - är en custom knapp som finns i index.css
+    */}
+    <p className="my-btn">jag är en text</p>
   </>
 }
