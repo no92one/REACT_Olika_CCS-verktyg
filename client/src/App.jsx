e4c4b56b29a0f980e11a5ecf40444bd0f1e47213
unknown <linus.lindroth.92@gmail.com>
2025-09-19 10:00:52 +0200

    added custom styling in materialize

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,14 @@
 export default function App() {
   return <>
     <h1 className="center-align">Homepage</h1>
-    <a className="waves-effect waves-light btn">Knapp</a>
+    {/* 
+      "my-margin" - är en custom margin som finns i index.css
+      Kan änvända "style" för att skapa custom css direkt i html-elementet 
+    */}
+    <a className="waves-effect waves-light btn my-margin">Knapp</a>
+    <a
+      className="waves-effect waves-light btn"
+      style={{ backgroundColor: '#22c55e' }}>Knapp 2
+    </a>
   </>
 }
