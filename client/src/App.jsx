8209fc8f2471fc75c8aabdd733f920a855caf8d5
unknown <linus.lindroth.92@gmail.com>
2025-09-19 09:50:07 +0200

    added custom css in bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,5 +4,10 @@ export default function App() {
   return <>
     <h1>Homepage</h1>
     <Button variant="primary">Click me</Button>
+    {/* 
+    "btn-mycolor"- är en egen custom färg som finns i index.css
+    "m-2" - är bootstrap, inte react-bootstrap, sätt att lägga till margin i komponenter
+    */}
+    <Button className='btn-mycolor m-2'>Click me also</Button>
   </>
 }
