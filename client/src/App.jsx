267295aa61642e7661f1e443eb18efe35de9d695
unknown <linus.lindroth.92@gmail.com>
2025-09-19 08:02:12 +0200

    added materialize-css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 export default function App() {
   return <>
-    <h1>Homepage</h1>
+    <h1 className="center-align">Homepage</h1>
+    <a className="waves-effect waves-light btn">Knapp</a>
   </>
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
