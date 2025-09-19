e3f03e2372959c181903f23b697c6e2cdbd8dd45
unknown <linus.lindroth.92@gmail.com>
2025-09-19 07:47:22 +0200

    added button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
+import Button from 'react-bootstrap/Button';
+
 export default function App() {
   return <>
     <h1>Homepage</h1>
+    <Button variant="primary">Click me</Button>
   </>
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
